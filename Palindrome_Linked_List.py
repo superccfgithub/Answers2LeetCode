954dcb32d80adcfa4e09c5ee865a02d09564d564
superccfgithub <chaofan126go@163.com>
2015-07-10 15:41:45 +0800

    Palindrome Linked List
    Palindrome Linked List.python

---
+++ Palindrome_Linked_List.py
@@ -0,0 +1,30 @@
+#question name:Palindrome Linked List
+#question link:https://leetcode.com/problems/palindrome-linked-list/
+#Runtime: 200 ms
+
+#-------------------Submitted Code-------------------------
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param {ListNode} head
+    # @return {boolean}
+    def isPalindrome(self, head):
+        l = list()
+        l.append(head)
+        if not head:
+            return True
+        if head.next == None:
+            return True
+        n = 1
+        while l[n-1].next != None:
+            l.append(l[n-1].next)
+            n = n + 1
+        lenth = len(l)
+        for i in range(0, lenth):
+            if l[i].val != l[lenth-1-i].val:
+                return False
+        return True
